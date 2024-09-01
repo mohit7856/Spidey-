7658dc02c3fc76de83afd454a6ca139318f09724
mohit7856 <mohitraj00029@gmail.com>
2024-09-01 07:28:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7255048090:AAFpMpH9KDNrr3BpnlAlraE5c2UkqhlSY5w")
+BOT_TOKEN=("7407497547:AAHyEtzp62osaCOtDt1E1wJfug0ZexrcLbY")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("")
+OWNER_USERNAME=("Mohitraj104")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("")
+ADMIN_IDS=("1825334364")
 
 
 
